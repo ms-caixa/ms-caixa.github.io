ef7fd2ae33fbaff670b5140ca479fdb687b6770f
Mairon Costa <maironmendes@yahoo.com.br>
2020-08-31 20:42:18 -0300

    change the cache strategy. now, the it goes first get assests from network and after get from cache.

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-const staticContagemValores = "contagem-valores-v1.1"
+const staticContagemValores = "contagem-valores-v1.2"
 const assets = [
     "/",
     "/index.html",
@@ -15,10 +15,62 @@ self.addEventListener("install", installEvent => {
     )
 });
 
-self.addEventListener("fetch", fetchEvent => {
-    fetchEvent.respondWith(
-        caches.match(fetchEvent.request).then(res => {
-            return res || fetch(fetchEvent.request)
+// self.addEventListener("fetch", fetchEvent => {
+//     fetchEvent.respondWith(
+//         caches.match(fetchEvent.request).then(res => {
+//             return res || fetch(fetchEvent.request)
+//         })
+//     )
+// })
+
+// Cache Strategy
+// https://blog.bitsrc.io/5-service-worker-caching-strategies-for-your-next-pwa-app-58539f156f52
+
+// Cache First, then Network Strategy
+// self.addEventListener('fetch', function (event) {
+//     event.respondWith(
+//         caches.open(cacheName)
+//             .then(function(cache) {
+//                 cache.match(event.request)
+//                     .then( function(cacheResponse) {
+//                         if(cacheResponse)
+//                             return cacheResponse
+//                         else
+//                             return fetch(event.request)
+//                                 .then(function(networkResponse) {
+//                                     cache.put(event.request, networkResponse.clone())
+//                                     return networkResponse
+//                                 })
+//                     })
+//             })
+//     )
+// });
+
+// Network First, then Cache Strategy
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        fetch(event.request).catch(function() {
+            return caches.match(event.request)
         })
     )
 })
+
+// Cache Only Strategy
+// self.addEventListener('fetch', function (event) {
+//     event.respondWith(
+//         caches.open(cacheName).then(function(cache) {
+//             cache.match(event.request).then(function(cacheResponse) {
+//                 return cacheResponse;
+//             })
+//         })
+//     )
+// })
+
+// Network Only Strategy
+// self.addEventListener('fetch', function (event) {
+//     event.respondWith(
+//         fetch(event.request).then(function(networkResponse) {
+//             return networkResponse
+//         })
+//     )
+// })
